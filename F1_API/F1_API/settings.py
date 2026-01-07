35284ae5cf850721a0461ee587354995d3f1a852
IreneGithundi <njokiirene4349@gmail.com>
2026-01-07 15:09:15 +0300

    Users Model and Auth endpoints

--- F1_API/F1_API/settings.py
+++ F1_API/F1_API/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'drivers.apps.DriversConfig',
     'trivia_challenge.apps.TriviaChallengeConfig',
     'users.apps.UsersConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.User'
